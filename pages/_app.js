eee66ba74141d91eb07f2a0c356cd37821268f33
Letisia Pangata'a <lpan375@aucklanduni.ac.nz>
2021-02-05 12:17:02 +1300

    added all files

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import '../components/RichText.css';
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
